c4d2afdb9ebfd125b62f0818f5d08377a64ddb9d
PossibleAtom <kieran_geraghty@hotmail.com>
2020-09-04 14:02:12 +1000

    Updates

---
+++ precache-manifest.6d6a80ccb0ddc5972327b25066c8b2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567489168015e307131637e2590e3e7f",
+    "url": "/digital-gm-screen/index.html"
+  },
+  {
+    "revision": "90b5aa669cef6567877d",
+    "url": "/digital-gm-screen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b24fc03e24add672c12b",
+    "url": "/digital-gm-screen/static/js/2.fde750f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digital-gm-screen/static/js/2.fde750f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b5aa669cef6567877d",
+    "url": "/digital-gm-screen/static/js/main.98170879.chunk.js"
+  },
+  {
+    "revision": "f6063e65091c14026ba9",
+    "url": "/digital-gm-screen/static/js/runtime-main.b87527d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/digital-gm-screen/static/media/logo.5d5d9eef.svg"
+  }
+]);
